5ac7c15b76f82f2aa12fc9f5f84a6e554e40a7a3
Ameb <biedma.64567@e.unavarra.es>
2016-12-27 11:58:59 +0100

    Eliminadas declaraciones innecesarias

--- ts.h
+++ ts.h
@@ -1,7 +1,5 @@
 #ifndef JJIB_TS_H
 #define JJIB_TS_H
-#define LONG_MAX_NOMBRE 16
-typedef enum etipo {entero, booleano, caracter, real, cadena} etipo;
 // estructura basica de nuestra lista enlazada
 struct nodo{
 	int sid;
